cb09bf1b7f51a0fc1e74ac8a3c371f97b762f8d6
H82M19\Student <Student>
2025-08-20 07:43:18 +0700

    Khoi tao du an

---
+++ dist/HelloWorld.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function hello(s) {
+    return `Hello ${s}`;
+}
+console.log(hello("Huy Hoang"));
---
+++ src/HelloWorld.ts
@@ -0,0 +1,5 @@
+function hello (s:string):string{
+    return `Hello ${s}`;
+}
+
+console.log(hello("Huy Hoang"))
